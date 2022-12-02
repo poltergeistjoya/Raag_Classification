13d763d1bc6896d5964ed260b3beb4f1209da46f
Ravindra Bisram <ravindrabisram@gmail.com>
2022-12-02 16:42:48 -0500

    f

--- main.py
+++ main.py
@@ -30,6 +30,14 @@ df = df.sample(frac=1).reset_index(drop=True)
 # List of dataframes, where each entry is a single batch
 list_df = [df[i:i+BATCH_SIZE] for i in range(0,df.shape[0],BATCH_SIZE)]
 
+# Create model
+model = model.simple_model(IMAGE_LEN, IMAGE_WIDTH, NUM_RAGAS, LAMBDA)
+
+# create optimizer, we use adam with a Learning rate of 1e-4
+opt = Adam(learning_rate = 1e-4)
+
+# Categorical Cross Entropy Loss Function
+cce = tf.keras.losses.CategoricalCrossentropy
 
 def train_step(batch):
     # Batch is a slice of the dataset, each sample consists of ('path to audio file', one_hot_class)
@@ -46,7 +54,6 @@ def train_step(batch):
     return loss_value
 
 def main():
-
     for epoch in range(1, EPOCHS+1):
         bar = tf.keras.utils.Progbar(len(list_df)-1)
         losses = []
@@ -63,11 +70,4 @@ def main():
 
 if __name__ == "__main__":
     print(list_df[0])
-    # Create model
-    model = model.simple_model(IMAGE_LEN, IMAGE_WIDTH, NUM_RAGAS, LAMBDA)
-
-    # create optimizer, we use adam with a Learning rate of 1e-4
-    opt = Adam(learning_rate = 1e-4)
-
-    # Categorical Cross Entropy Loss Function
-    cce = tf.keras.losses.CategoricalCrossentropy
+    main()
